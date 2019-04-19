e3c1bcff3120b1a2fc0ff1ce404a29b063605dd7
github username <github email address>
2019-04-18 20:47:14 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,56 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.all.select do |captain| 
+      boat_is_catamaran = false
+      captain.boats.each do |boat|
+        if boat.classifications.exists?(name: "Catamaran")
+          boat_is_catamaran = true
+        end 
+      end 
+      boat_is_catamaran
+    end 
+  end
+
+  def self.sailors
+    self.all.select do |captain| 
+      boat_is_catamaran = false
+      captain.boats.each do |boat|
+        if boat.classifications.exists?(name: "Sailboat")
+          boat_is_catamaran = true
+        end 
+      end 
+      boat_is_catamaran
+    end 
+  end
+
+  def self.talented_seafarers
+    self.all.select do |captain| 
+      boat_is_sailboat = false
+      boat_is_motorboat = false
+      captain.boats.each do |boat|
+        if boat.classifications.exists?(name: "Sailboat") 
+          boat_is_sailboat = true
+        end 
+        if boat.classifications.exists?(name: "Motorboat")
+          boat_is_motorboat = true
+        end
+      end 
+      boat_is_sailboat && boat_is_motorboat
+    end 
+  end
+
+  def self.non_sailors
+    self.all.select do |captain| 
+      boats_are_sailboats = false
+      captain.boats.each do |boat|
+        if boat.classifications.exists?(name: "Sailboat")
+          boats_are_sailboats = true
+        end 
+      end 
+      !boats_are_sailboats
+    end 
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,22 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all.uniq {|classification| classification.name}
+  end 
+
+  def self.longest
+    longest = 0
+    classifications = nil
+    self.all.each do |classification|
+      classification.boats.each do |boat|
+        if longest < boat.length
+          longest = boat.length
+          classifications = boat.classifications
+        end 
+      end 
+    end 
+    classifications
+  end 
 end
